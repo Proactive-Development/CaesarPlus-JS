562b50b29061d65b01c54fe743757a902d908961
awesomelewis2007 <awesomelewis2007@gmail.com>
2022-04-03 18:34:53 +0100

    Add testing

---
+++ tests/caesarplus.test.js
@@ -0,0 +1,13 @@
+const caesarplus = require('../src/caesarplus');
+test('Encode Hello World ', () => {
+    var encrypted = caesarplus.encrypt("Hello World!");
+    if(JSON.parse(encrypted).text == "Hello World!") {
+        fail("Encryption failed");
+    }
+});   
+test('Decode and encrypted message', () => {
+    var decrypted =caesarplus.decrypt(JSON.parse(encrypted).text, JSON.parse(encrypted).key);
+    if(JSON.parse(decrypted).text != "Hello World!") {
+        fail("Decryption failed");
+    }
+});
--- tests/test.js
+++
@@ -1,5 +0,0 @@
-const caesarplus = require('../src/caesarplus');
-var encrypted = caesarplus.encrypt("Hello World!");
-console.log(JSON.parse(encrypted).text);
-console.log(JSON.parse(encrypted).key);
-console.log(caesarplus.decrypt(JSON.parse(encrypted).text, JSON.parse(encrypted).key));
